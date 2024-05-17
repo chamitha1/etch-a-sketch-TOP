d0e09a37150f54995de58c839e0e0878af3cb2da
Chamitha <chamithashelan@gmail.com>
2024-05-17 12:53:52 +0530

    Modified the program entirely. Added two new funct ions [createContainer() and setEventListener()].
    They will create the grid of required size and
    set the eventListners for each box in the grid
    respectively.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const container = document.querySelector(".container");
+function createGrid(size = 256){
+    const container = document.querySelector(".container");
+    for(let i=0; i<size; i++){
+        const box = document.createElement("div");
+        box.setAttribute("class", "box");
+        container.appendChild(box);
+    }
 
-
-
-for(let i=0; i<256; i++){
-    const div = document.createElement("div");
-    container.append(div);
-    div.setAttribute("class","box");
+    return container;
 }
 
-const boxes = document.querySelectorAll(".box");
-
-for(let i=0; i<boxes.length;i++){
-    console.log(boxes[i].className);
-
-    boxes[i].addEventListener("mouseover",()=>{
-        boxes[i].style.backgroundColor = "lightBlue";
-    });
+function setEventListener(container){
+    const boxes = document.querySelectorAll(".box");
+    
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].addEventListener("mouseover", ()=>{
+            boxes[i].style.backgroundColor = "lightblue";
+        });
+    }
 }
+
+
+
+const container = createGrid();
+setEventListener(container);
