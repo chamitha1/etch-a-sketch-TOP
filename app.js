7497b5aba50e37bb2e548be87a0aa8053dddeead
Chamitha <chamithashelan@gmail.com>
2024-05-17 16:59:41 +0530

    Added 4 new buttons. -toggle shading     Increases the opacity of the box by 10%.
    -toggle color mode
        Random color is generated for each box
    
    -toggle default
        Greyish color
    
    -eraser
        Color white

--- app.js
+++ app.js
@@ -11,14 +11,48 @@ function createGrid(size = 256,width_height = 30){
     return container;
 }
 
-function setEventListener(container){
+function toggleDefaultMode(container){
     const boxes = document.querySelectorAll(".box");
     
     for(let i=0; i<boxes.length; i++){
         boxes[i].addEventListener("mouseover", ()=>{
-            boxes[i].style.backgroundColor = "lightblue";
+            boxes[i].style.backgroundColor = "grey";
         });
     }
+}
+
+function toggleEraser(container){
+    const boxes = document.querySelectorAll(".box");
+    
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].addEventListener("mouseover", ()=>{
+            boxes[i].style.backgroundColor = "white";
+        });
+    } 
+}
+
+function toggleColor(container){
+    const boxes = document.querySelectorAll(".box");
+    
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].addEventListener("mouseover", ()=>{
+            boxes[i].style.backgroundColor = "rgb(" + randomInt() +","
+                             +randomInt() + "," + randomInt() + ")";
+        });
+    }
+}
+
+function toggleShading(container){
+    const boxes = document.querySelectorAll(".box");
+
+    for(let i=0; i<boxes.length; i++){
+        let value = 10;
+
+        boxes[i].addEventListener("mouseover", ()=>{
+            boxes[i].style.opacity = value +"%";
+            value += 10;
+        });
+    } 
 }
 
 function removeBoxes(){
@@ -30,24 +64,62 @@ function removeBoxes(){
 }
 
 function newGrid(){
-    let input = Number(prompt("Enter a size for the grid( <100 ) "));
+    let input;
+    do{
+        input = Number(prompt("Enter a size for the grid( <100 ) "));
+        if(input > 100){
+            continue;
+        }else{
+            break;
+        }
+    }while(true);
+
     let size = input * input;
     let width_height = 480/input;
 
     width_height += "px";
-    console.log(width_height);
-
     const newContainer = createGrid(size, width_height);
 
-}   
+}
 
-const btn = document.querySelector("#btn-newGrid");
+//generating a number between 0 - 254
+function randomInt(){
+    return Math.floor(Math.random() * 254);
+
+}
+
+const btn_newGrid = document.querySelector("#btn-newGrid");
+const btn_eraser = document.querySelector("#btn-eraser");
+const btn_default = document.querySelector("#btn-default");
+const btn_color = document.querySelector("#btn-color");
+const btn_shading = document.querySelector("#btn-shading");
 
 const container = createGrid();
-setEventListener(container);
+toggleDefaultMode(container);
 
-container = btn.addEventListener("click", ()=>{
+btn_newGrid.addEventListener("click", ()=>{
     removeBoxes();
     const newContainer = newGrid();
-    setEventListener(newContainer);
+    toggleDefaultMode(newContainer);
 });
+
+
+btn_eraser.addEventListener("click", ()=>{
+    toggleEraser(container);
+});
+
+btn_shading.addEventListener("click", ()=>{
+    toggleShading(container);
+});
+
+btn_default.addEventListener("click", ()=>{
+    toggleDefaultMode(container);
+});
+
+btn_color.addEventListener("click", ()=>{
+    toggleColor(container);
+});
+
+
+
+console.log(randomInt());    
